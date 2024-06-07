98faff79f5d23239b07497c0ef1bca1a086d19d6
Lavendar Solution <lavendarsolution@gmail.com>
2024-06-07 07:35:21 -0400

    feat: fix minor issue

--- components/dashboard/tickets/page.tsx
+++ components/dashboard/tickets/page.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MarkTicket from "./markTicket"
+import {Sezzle, SheerMark} from "./ticketDisplay";
 interface TicketProps {
     className?: string
 }
@@ -12,20 +13,15 @@ const Ticket:React.FC<TicketProps> = (props) => {
     const [sezzle, setSezzle] = useState<Boolean>(false);
     const [shipping, setShipping] = useState<Boolean>(false);
 
-    const handleShow = () => {
-
-    }
-
     return (
         <div className={`${props.className} lg:px-32 px-10`}>
             <div className="grid lg:grid-cols-3 lg:gap-24 gap-5 w-full">
-                <MarkTicket mark="https://damnedventures.com/wp-content/uploads/4payments6weeks0interest-white-1.png" />
-                <MarkTicket mark="https://damnedventures.com/wp-content/uploads/1_jLAS3yLE6G-ERUcNhAdWUA-Copy-1.png" />
-                <MarkTicket mark="https://damnedventures.com/wp-content/uploads/Asset-4-1.png" />
+                <MarkTicket mark="https://damnedventures.com/wp-content/uploads/4payments6weeks0interest-white-1.png" text="Buy now, Pay Later. 0% interest" onShow={setSezzle} />
+                <MarkTicket mark="https://damnedventures.com/wp-content/uploads/1_jLAS3yLE6G-ERUcNhAdWUA-Copy-1.png" text="Free shipping on US orders over $100" onShow={setShipping} />
+                <MarkTicket mark="https://damnedventures.com/wp-content/uploads/Asset-4-1.png" text="Military and first responder discounts" onShow={setSheerMark} />
             </div>
-            <div className={`${!sheerMark && 'hidden'} fixed top-0 left-0 flex justify-center items-center px-96 py-60 w-screen h-[100vh]`}>
-
-            </div>            
+            {sezzle && <Sezzle onShow={setSezzle} />}
+            {sheerMark && <SheerMark onShow={setSheerMark} />}
         </div>
             
     )
