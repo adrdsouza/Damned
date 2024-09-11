d02da730accc27c78684a9c1eadde484c5525cd5
fareedkamal <fkamalars@gmail.com>
2024-09-11 15:57:53 +0500

    fixed build errors

--- app/shop/edc/page.tsx
+++ app/shop/edc/page.tsx
@@ -9,31 +9,41 @@ export const metadata: Metadata = {
     'EDC tools, expertly crafted with great attention to detail. Damned Designs is where breathtaking aesthetics meets uncompromising functionality.',
 };
 
-const Edc: React.FC = async () => {
-  const { nodes: products } = await fetchProducts({
-    first: 50,
-    where: { categoryId: 1143 },
-  });
+const Edc = async () => {
+  try {
+    const { nodes: products } = await fetchProducts({
+      first: 50,
+      where: { categoryId: 1143 },
+    });
 
-  return (
-    <div>
-      <DashboardBack page='edc' />
+    // If products are null or undefined, set it to null
+    if (!products) {
+      return <div>No products found</div>;
+    }
 
-      <div className='flex m-auto px-8 w-full h-[400px] md:h-screen'>
-        <div className='text-white my-auto text-center md:text-left'>
-          <p className='text-2xl md:text-3xl mb-2'>POCKET ART</p>
-          <p className='text-base mb-4'>
-            From knucks to beads from patches to coins. This is where you’ll
-            find them.
-          </p>
+    return (
+      <div>
+        <DashboardBack page='edc' />
+
+        <div className='flex m-auto px-8 w-full h-[400px] md:h-screen'>
+          <div className='text-white my-auto text-center md:text-left'>
+            <p className='text-2xl md:text-3xl mb-2'>POCKET ART</p>
+            <p className='text-base mb-4'>
+              From knucks to beads from patches to coins. This is where you’ll
+              find them.
+            </p>
+          </div>
+        </div>
+
+        <div className='flex m-auto px-8 min-h-[500px] h-full py-8 bg-white w-full'>
+          <ProductsList data={products} showPagination={true} />
         </div>
       </div>
-
-      <div className='flex m-auto px-8 min-h-[500px] h-full py-8 bg-white w-full'>
-        <ProductsList data={products} showPagination={true} />
-      </div>
-    </div>
-  );
+    );
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    return <div>No products found</div>;
+  }
 };
 
 export default Edc;
--- app/shop/fixed-blade-knives/page.tsx
+++ app/shop/fixed-blade-knives/page.tsx
@@ -7,16 +7,25 @@ export const metadata: Metadata = {
 };
 
 const FixedBladeKnives: React.FC = async () => {
-  const { nodes: products } = await fetchProducts({
-    first: 30,
-    where: { categoryId: 1266 },
-  });
+  try {
+    const { nodes: products } = await fetchProducts({
+      first: 30,
+      where: { categoryId: 1266 },
+    });
 
-  return (
-    <div className='w-full h-full min-h-[500px] px-8 py-4 m-auto'>
-      <ProductsList data={products} showPagination={true} />
-    </div>
-  );
+    if (!products) {
+      return <div>No products found</div>;
+    }
+
+    return (
+      <div className='w-full h-full min-h-[500px] px-8 py-4 m-auto'>
+        <ProductsList data={products} showPagination={true} />
+      </div>
+    );
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    return <div>No products found</div>;
+  }
 };
 
 export default FixedBladeKnives;
