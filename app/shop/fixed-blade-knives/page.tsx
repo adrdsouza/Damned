95f8b95aa8da18b1498bd6c8ae3c0282af77b085
fareedkamal <fkamalars@gmail.com>
2024-09-11 22:05:21 +0500

    new updates and build fixes

--- app/shop/fixed-blade-knives/page.tsx
+++ app/shop/fixed-blade-knives/page.tsx
@@ -2,30 +2,23 @@ import ProductsList from '@/components/shop/products-listing';
 import { fetchProducts } from '@/graphql';
 import { Metadata } from 'next';
 
+export const dynamic = 'force-dynamic';
+
 export const metadata: Metadata = {
   title: 'Fixed Blade Knives - Damned Designs',
 };
 
 const FixedBladeKnives: React.FC = async () => {
-  try {
-    const { nodes: products } = await fetchProducts({
-      first: 30,
-      where: { categoryId: 1266 },
-    });
+  const { nodes: products } = await fetchProducts({
+    first: 30,
+    where: { categoryId: 1266 },
+  });
 
-    if (!products) {
-      return <div>No products found</div>;
-    }
-
-    return (
-      <div className='w-full h-full min-h-[500px] px-8 py-4 m-auto'>
-        <ProductsList data={products} showPagination={true} />
-      </div>
-    );
-  } catch (error) {
-    console.error('Error fetching products:', error);
-    return <div>No products found</div>;
-  }
+  return (
+    <div className='w-full h-full min-h-[500px] px-8 py-4 m-auto'>
+      <ProductsList data={products} showPagination={true} />
+    </div>
+  );
 };
 
 export default FixedBladeKnives;
