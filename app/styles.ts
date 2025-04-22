8244d7a4e933efc79e7139ba8d74d558a9b0b1a2
adrdsouza <102761952+adrdsouza@users.noreply.github.com>
2025-04-22 19:38:16 +0100

    reset

--- app/styles.ts
+++
@@ -1,7 +0,0 @@
-export const text = {
-  sm: 'text-[8px] sm:text-[12px]',
-  md: 'text-[12px] sm:text-[16px]',
-  lg: 'text-[22px] sm:text-[32px]',
-};
-
-export const pallete = ['#a89c9c', '#8b8080'];
